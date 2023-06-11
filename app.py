5c058a6f23f639cf3efb38b6cb9af3f515e61b42
Tonykehl <malelafila@gmail.com>
2023-06-11 19:41:41 +0100

    my_first_commit

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
+import requests
 
 response = requests.post("https://my_app.herokuapp.com/predict", json={
     "input": [[7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.001, 3.0, 0.45, 8.8]]
